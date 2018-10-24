c8618bc9bbde160c29f5828ccf10eaca4805a4a9
twocucao <twocucao@gmail.com>
2018-10-24 10:57:02 +0800

    polish pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
+import pytest
+import random
 
+
+@pytest.fixture
+def items_to_be_sorted():
+    items = list(range(1, 1000))
+    random.shuffle(items)
+    return items
+
+
+@pytest.fixture
+def items_expected_sorted():
+    return list(range(1, 1000))
